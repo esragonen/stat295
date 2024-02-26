b2b20193f06b0f3c8ff5323357374a1015fcdc82
esragonen <gonen.esra@metu.edu.tr>
2024-02-26 13:42:53 +0300

    stat lecture

---
+++ lectue1.R
@@ -0,0 +1,17 @@
+install.packages(c("devtools","roxygen2","testthat","rmarkdown","pkgdown","purrr"))
+
+usethis::use_git_config(user.name = "esragonen",
+                        user.email="gonen.esra@metu.edu.tr")
+
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+usethis::git_sitrep()
+
+
+
+
+
+
+
+
+
